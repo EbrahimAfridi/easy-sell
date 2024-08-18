c592faa61b6572f25535b2078c40377a60707de6
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-08-18 14:16:18 +0530

    add card, badge and submit button component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {children}
+        <div className="bg-gray-951 py-12 px-4">{children}</div>
+        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
+import Card from "@/components/card";
 
 export default function Home() {
+  const products = [
+    {
+      id: 0,
+      name: "Product Name",
+      description: "Product Description",
+      price: 0,
+      imageUrl: "https://via.placeholder.com/300",
+    },
+  ];
+  
   return (
     <main className="min-h-screen mx-auto max-w-[100rem]">
+      {products.map((product) => (
+        <Card key={product.id} {...product} />
+      ))}
     </main>
   );
 }
