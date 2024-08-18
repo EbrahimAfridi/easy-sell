563a854fd26e08e32d8a5205d1a1d4ca56e515c8
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-08-18 14:42:17 +0530

    integrate supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,30 @@
 import Card from "@/components/card";
+import { notFound } from "next/navigation";
 
-export default function Home() {
-  const products = [
-    {
-      id: 0,
-      name: "Product Name",
-      description: "Product Description",
-      price: 0,
-      imageUrl: "https://via.placeholder.com/300",
-    },
-  ];
-  
+export const revalidate = 0;
+
+export default async function Home() {
   return (
-    <main className="min-h-screen mx-auto max-w-[100rem]">
-      {products.map((product) => (
-        <Card key={product.id} {...product} />
-      ))}
+    <main className="min-h-screen max-w-[100rem] mx-auto">
+      <div className="px-12 pt-12 pb-20">
+        <div className="flex flex-col xl:flex-row gap-2 xl:gap-40">
+          <div className="pt-12">
+            <h2 className="text-4xl mb-16">OUR TOP PRODUCTS</h2>
+            <p className="text-xl">You can pay to boost your products here.</p>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-12"></div>
+        </div>
+
+        <h2 className="text-4xl mt-20 mb-16">ALL PRODUCTS</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+            {products.map((item, idx) => (
+              <Card id={item.id} key={`${item.name}-${idx}`} {...item} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-xl text-gray-800">All our products are gone...</p>
+        )}
+      </div>
     </main>
   );
 }
